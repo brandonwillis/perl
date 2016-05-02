7aa72e70f1e06e0c1072e6d3ca933e157449d73a
Brandon Willis <brandonlynnwillis@gmail.com>
2016-05-02 11:40:48 -0700

    [feat] student dashboard now requires token

--- client/student/studentdashboard.js
+++ client/student/studentdashboard.js
@@ -1,10 +1,22 @@
-angular.module('Perl.studentDashboard', ['ngMaterial', 'ngMdIcons'])
+angular.module('Perl.studentDashboard', ['ngMaterial', 'ngMdIcons', 'firebase'])
 
-.controller('studentDashboard',function($mdMedia, $scope, studentFactory){
+.controller('studentDashboard',function($mdMedia, $scope, studentFactory, $state, $firebaseAuth){
 	$scope.studentName;
 	$scope.studentLocation;
 	$scope.studentBio;
-
+	$scope.ref = new Firebase("https://perl-thesis.firebaseio.com/");
+	$scope.authObj = $firebaseAuth($scope.ref);
+	$scope.checkAuthentication = function() {
+		$scope.authObj.$onAuth(function(authData) {
+			console.log("my authData is: ", authData);
+			if(authData) {
+				$scope.uid = authData.auth.uid;
+			} else {
+				$state.go('signin');
+			}
+		})
+	}
+	$scope.checkAuthentication();
 	// $scope.getStudentProfile = function() {
 	// 	studentFactory.getProfile()
 	// 	.then(function(data){
